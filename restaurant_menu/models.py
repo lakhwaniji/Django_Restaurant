f75b51881c3ba77bb7a989acde7ed8e39ed8528a
lakhwaniji <lakhwanilove@gmail.com>
2023-06-29 12:46:36 +0400

    Initial commit

---
+++ restaurant_menu/models.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+MEAL_TYPE=(
+    ("starters","Starters"),
+    ("salads","Salads"),
+    ("main_dishes","Main_dishes"),
+    ("desserts","Desserts")
+)
+
+STATUS=(
+    (0,"Unavailable"),
+    (1,"Available")
+)
+
+class Item(models.Model):
+    meal=models.CharField(max_length=1000,unique=True)
+    description=models.CharField(max_length=2000)
+    price=models.DecimalField(decimal_places=2,max_digits=10)
+    meal_type=models.CharField(choices=MEAL_TYPE,max_length=200)
+    author=models.ForeignKey(User,on_delete=models.PROTECT)
+    status=models.IntegerField(choices=STATUS,default=1)
+    date_created=models.DateTimeField(auto_now_add=True)
+    date_updated=models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.meal}"
---
+++ restaurant_menu/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views import generic
+from .models import Item, MEAL_TYPE
+
+
+class MenuList(generic.ListView):
+    queryset = Item.objects.order_by("date_created")
+    template_name = "Index.html"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context["meals"]=MEAL_TYPE
+        return context
+
+
+class MenuItemDetails(generic.DetailView):
+    model=Item
+    template_name = "menu_item_detail.html"
